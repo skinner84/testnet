bf4ed4bd7a6dba16dbc4f1503d051e1421f8daca
skinner84 <jarman.lucas@gmail.com>
2018-01-14 12:34:59 +1100

    third commit

--- cmc.py
+++ cmc.py
@@ -1,4 +1,12 @@
 from coinmarketcap import Market
 import json
+import requests
 
+url = "https://api.coinmarketcap.com/v1/ticker/bitcoin"
 
+myResponse = requests.get(url)
+print (myResponse)
+
+data = myResponse.json()
+
+print (data)
